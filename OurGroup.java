a76026c0c77f6d8b58b1747bb4fc4a45ae5528af
AchitaPoomanee <78794222+AchitaPoomanee@users.noreply.github.com>
2021-02-09 19:00:32 +0700

    Update OurGroup.java

--- OurGroup.java
+++ OurGroup.java
@@ -8,6 +8,7 @@ public class OurGroup {
         groupMembers = new Vector<String>();
         groupMembers.add("Tanutcha"); //PUT YOUR NAME HERE
 	groupMembers.add("Nalinthip"); 
+	groupMembers.add("Achita");
     }
     public Vector<String> getGroupMembers () {
         return groupMembers;
